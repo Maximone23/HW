e93ced00f10e7b057b3984b934e9372e01f9d675
Maximone23 <maxim.one23@gmail.com>
2020-12-10 21:43:45 +0500

    Updates

---
+++ precache-manifest.3df29b7c7cdc63a097e92e5bd06d7be4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312078fb220ce775ca8995fd19542b4e",
+    "url": "/HW/index.html"
+  },
+  {
+    "revision": "2fabef1850bde5fbf8ad",
+    "url": "/HW/static/css/main.890b2917.chunk.css"
+  },
+  {
+    "revision": "cbd90500a71a823529fb",
+    "url": "/HW/static/js/2.d88f168f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HW/static/js/2.d88f168f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fabef1850bde5fbf8ad",
+    "url": "/HW/static/js/main.c5a88f1b.chunk.js"
+  },
+  {
+    "revision": "f4e08d7fea8dae7a3704",
+    "url": "/HW/static/js/runtime-main.9e739d5d.js"
+  }
+]);
